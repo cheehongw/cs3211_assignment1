e4de5a8a3008f5d5383dd6174c46a8c2b2a3e624
Wong Chee Hong <c.h.wong2606@gmail.com>
2023-02-20 02:04:49 +0800

    fix comparator order

--- Order.cpp
+++ Order.cpp
@@ -3,13 +3,13 @@
 bool operator<(const Order &lhs, const Order &rhs)
 {
     if (lhs.is_sell_order) {
-        return lhs.price == rhs.price 
+        return !(lhs.price == rhs.price 
             ? lhs.time_order_added < rhs.time_order_added 
-            : lhs.price > rhs.price;
+            : lhs.price > rhs.price);
     } else {
-        return lhs.price == rhs.price 
+        return !(lhs.price == rhs.price 
             ? lhs.time_order_added < rhs.time_order_added 
-            : lhs.price < rhs.price;
+            : lhs.price < rhs.price);
     }
 }
 
@@ -19,13 +19,13 @@ update_result Order::update_order(uint32_t reduce_by)
     update_result res;
 
     if (reduce_by >= count) {
-        count = 0;
         res.units_traded = count;
+        count = 0;
         res.is_resting_order_fully_filled = true;
 
     } else {
-        count = count - reduce_by;
         res.units_traded = reduce_by;
+        count = count - reduce_by;
         res.is_resting_order_fully_filled = false;
     }
 
